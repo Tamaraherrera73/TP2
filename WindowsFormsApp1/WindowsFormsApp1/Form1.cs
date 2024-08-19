91dae919dbf63314c3db0193a5f6563e3fe1d5cf
TamaraHerrera73 <therrera@docentes.frgp.utn.edu.ar>
2024-08-19 20:31:28 -0300

    Commit por falla

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -20,7 +20,7 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hola Mundo");
-            textBox1.Text = "HolaMundo";
+            textBox1.Text = "Hola Mundo";
         }
     }
 }
